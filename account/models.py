8161ea9270a678708238ddb3f4c6e9e81679b987
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-28 21:51:52 +0900

    defined account user model

--- account/models.py
+++ account/models.py
@@ -8,3 +8,44 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = ASCIIUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exist."),
+        },
+    )
+    email = models.EmailField(_('email address'), blank=True)
+    profile_icon = models.ImageField(
+        _('profile icon'), upload_to='profile_icons', null=True, blank=True)
+
+    self_introduction = models.CharField(
+        _('self introduction'), max_length=512, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    is_active = models.BooleanField(_('active'), default=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        db_table = 'users'
+        swappable = 'AUTH_USER_MODEL'
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
